c821fb7103551e8f246a854eb1828f9dc0fae6d5
Ronaldo Noberto <rns6@aluno.ifal.edu.br>
2022-06-19 02:38:23 -0300

    exercício 04 feito

---
+++ Exercicios/Aula07/Ex04/script.js
@@ -0,0 +1,13 @@
+var xa=6;
+var ya=0;
+
+var xb=3;
+var yb;
+
+var xc=0;
+var yc=6;
+
+var xm = (xa + xc)/2;
+var ym =(ya + yc)/2;
+
+alert("O ponto B é formado pela abscissa "+xm+" e ordenada "+ym);
